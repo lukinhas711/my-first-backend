c35aab8b53119eb78e2e424df4904851605bb061
Lucas <lucas.ribeiro711@gmail.com>
2022-11-06 12:38:48 -0300

    chore(config): tsconfig

--- src/server.spec.ts
+++ src/server.spec.ts
@@ -1,4 +1,4 @@
-import sayHelloWorld from "./server";
+import sayHelloWorld from "@/server";
 
 describe("Testing my hallo word", () => {
   it("Should call sayHelloWorld and return my msg", () => {
